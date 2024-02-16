0a44f554644dccd59f9faf62f83b461cca64cf55
Aaditya Pal <129619636+palAaditya@users.noreply.github.com>
2024-02-16 22:50:26 +0530

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,27 @@
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('show-x');
+    ul.classList.toggle('show');
+    // ul.classList.toggle('show');
+});
+
+
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
